b0ef022cc40d3e86a482ad348e05f1aadaae8441
JenniferLang1921 <jenniferlang1921@gmail.com>
2018-04-22 15:11:48 -0500

    working!

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+var friendList = [
+    {
+       name:"Susan",
+       photo:"https://53744bf91d44b81762e0-fbbc959d4e21c00b07dbe9c75f9c0b63.ssl.cf3.rackcdn.com/media/EE/EEDD6F6F-6DAF-4301-A455-B86493C74F4C/Presentation.Large/male-pacific-walrus-in-water-at-haul-out.jpg",
+       scores:[2,3,4,5,1,2,3,4,5,5]
+    },
+    {
+       name:"Jason",
+       photo:"https://53744bf91d44b81762e0-fbbc959d4e21c00b07dbe9c75f9c0b63.ssl.cf3.rackcdn.com/media/EE/EEDD6F6F-6DAF-4301-A455-B86493C74F4C/Presentation.Large/male-pacific-walrus-in-water-at-haul-out.jpg",
+       scores:[4,2,5,1,3,2,2,1,3,2]
+    }
+    
+ ]
+ 
+ module.exports = friendList;
