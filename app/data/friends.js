a0c9afaabfa2a40894f52ad8241012cd7b1b8a3a
Jennifer Lang <jenniferlang1921@gmail.com>
2018-04-24 13:05:26 -0500

    more data, questions, bootstrap4

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,9 +4,33 @@ var friendList = [
        photo:"https://53744bf91d44b81762e0-fbbc959d4e21c00b07dbe9c75f9c0b63.ssl.cf3.rackcdn.com/media/EE/EEDD6F6F-6DAF-4301-A455-B86493C74F4C/Presentation.Large/male-pacific-walrus-in-water-at-haul-out.jpg",
        scores:[2,3,4,5,1,2,3,4,5,5]
     },
+
     {
-       name:"Jason",
-       photo:"https://53744bf91d44b81762e0-fbbc959d4e21c00b07dbe9c75f9c0b63.ssl.cf3.rackcdn.com/media/EE/EEDD6F6F-6DAF-4301-A455-B86493C74F4C/Presentation.Large/male-pacific-walrus-in-water-at-haul-out.jpg",
+        name:"Bob",
+        photo:"https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/photoark-lion.ngsversion.1466004832449.png",
+        scores:[1,1,1,1,1,1,1,1,1,1]
+     },
+
+     {
+        name:"Jill",
+        photo:"https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2561,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1494008886/articles/2017/04/03/the-internet-s-most-famous-pregnant-giraffe-still-hasn-t-given-birth/170403-jones-pregnant-giraffe-tease_alsbhr",
+        scores:[5,5,5,5,5,5,5,5,5,5]
+     },
+
+     {
+        name:"Dmitri",
+        photo:"https://thefinanser.com/wp-content/uploads/2015/12/6a01053620481c970b01b7c7617a9f970b-600wi.jpg",
+        scores:[1,2,3,4,5,1,2,3,4,5]
+     },
+
+     {
+        name:"Chloe",
+        photo:"http://www.tehcute.com/pics/201202/baby-swan-big.jpg",
+        scores:[2,2,2,2,2,2,2,2,2,2]
+     },
+    {
+       name:"Tyler",
+       photo:"https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Nature/Q-Z/siberian-tiger-profile.ngsversion.1478731451784.adapt.470.1.jpg",
        scores:[4,2,5,1,3,2,2,1,3,2]
     }
     
